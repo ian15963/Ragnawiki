4e7eaa99f1b0755a755265e3f7e0ea7890f67a0c
Ian Silva Santos <ian.santos@sptech.school>
2023-11-07 07:52:46 -0300

    Fazendo a modelagem lógica e alterando um pouco o modelo físico

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -15,20 +15,35 @@ CREATE TABLE usuario(
 INSERT INTO usuario VALUES (null, "ian@gmail.com", "Ian", "Silva Santos", "12345");
 SELECT nome, email FROM usuario WHERE email = 'ian@gmail.com' AND senha = "12345";
 
+CREATE TABLE bonus(
+	idBonus int primary key auto_increment,
+    Forca int,
+    Agilidade int,
+    Vitalidade int,
+    Destreza int,
+    Sorte int
+);
+
 CREATE TABLE classe(
 	idClasse int primary key auto_increment,
     nome varchar(45),
     tipo varchar(30),
     descricao varchar(1200),
-    classeAnterior int,
-    CONSTRAINT fkClasseAnterior FOREIGN KEY (classeAnterior) REFERENCES classe(idClasse)
+    fkBonus int,
+    CONSTRAINT fkBonus FOREIGN KEY (fkBonus) REFERENCES bonus(idBonus)
 );
 
 CREATE TABLE habilidade(
 	idHabilidade int primary key auto_increment,
-    nome varchar(45),
-    fkClasse int,
-    CONSTRAINT fkClasse FOREIGN KEY (fkClasse) REFERENCES classe(idClasse)
+    nome varchar(45)
+);
+
+CREATE TABLE classe_habilidade(
+	fkClasse int,
+    fkHabilidade int,
+    primary key(fkClasse, fkHabilidade),
+    CONSTRAINT fkClasseHabilidade FOREIGN KEY (fkClasse) REFERENCES classe(idClasse),
+    CONSTRAINT fkHabilidadeClasse FOREIGN KEY (fkHabilidade) REFERENCES habilidade(idHabilidade)
 );
 
 SELECT h.nome as 'Nome da Skill', c.nome as 'Nome da Classe', c.descricao FROM habilidade as h 
